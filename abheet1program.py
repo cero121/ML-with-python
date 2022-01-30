fcf8abca450d3749be5c213478bcc8bbfc831a98
cero121 <98686660+cero121@users.noreply.github.com>
2022-01-30 15:32:51 +0530

    intro

---
+++ abheet1program.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+Thapar_batch = [1,2,3,4,5,6,7,8]
+print(Thapar_batch)
+import matplotlib.pyplot as pt
+pt.plot(Thapar_batch)
